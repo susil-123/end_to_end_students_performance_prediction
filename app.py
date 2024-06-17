ab27ce71c79cb362a74f97d8079432cbe454d307
susil kumar <susilkumarkct@gmail.com>
2024-06-17 17:04:57 +0530

    updated version

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask,request,url_for,render_template
+from src.pipelines.predict_pipeline import CustomData,PredictPipeline
+from src.utils import load_object
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def form():
+    return render_template('index.html',result="")
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    gender = request.form.get('gender')
+    race_ethnicity = request.form.get('ethnicity')
+    parental_level_of_education = request.form.get('parental_level_of_education')
+    lunch = request.form.get('lunch')
+    test_preparation_course = request.form.get('test_preparation_course')
+    reading_score = int(request.form.get('reading_score'))
+    writing_score = int(request.form.get('writing_score'))
+    cd = CustomData(gender, race_ethnicity, parental_level_of_education, lunch, test_preparation_course, reading_score, writing_score)
+    df = cd.get_data_as_data_frame()
+    pp = PredictPipeline()
+    prediction = pp.predict(df)
+    return render_template('index.html',result=prediction[0])
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=5001)
--- src/pipelines/predict_pipeline.py
+++ src/pipelines/predict_pipeline.py
@@ -13,10 +13,10 @@ class PredictPipeline:
         try:
             model_path=os.path.join("artifacts","model.pkl")
             preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
-            print("Before Loading")
+            # print("Before Loading")
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
-            print("After Loading")
+            # print("After Loading")
             data_scaled=preprocessor.transform(features)
             preds=model.predict(data_scaled)
             return preds
